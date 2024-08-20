48887653ba113cfafe3866ce7dba95434617bcb6
Hamadullah12 <hamadfms05@gmail.com>
2024-08-20 12:34:16 +0500

    data types

---
+++ Day-1/class-1.js
@@ -0,0 +1,18 @@
+// console.log("Hello");
+// console work on the principle of "REPL"(Read Evaluate Print Loop)
+
+// VARIABLES
+
+// it is defined as the container that store the data
+// we can use varible for old browser
+// var car = audi;
+
+
+// DATA TYPES
+
+// there are 7 different data tyoes
+// Number, string , null, boolean, undefined, bigint, symbol
+
+// price = 7896;
+// if we dont know that what tyoe of data is it so we will simply type "typeof price" in console ut will give us the data
+
