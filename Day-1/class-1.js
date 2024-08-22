b164ebf835d05ff078b612de07ac9ae0820458db
Hamadullah12 <hamadfms05@gmail.com>
2024-08-22 20:25:58 +0500

    boolean topic discussed

--- Day-1/class-1.js
+++ Day-1/class-1.js
@@ -13,6 +13,60 @@
 // there are 7 different data tyoes
 // Number, string , null, boolean, undefined, bigint, symbol
 
+// TYPEOF   
 // price = 7896;
 // if we dont know that what tyoe of data is it so we will simply type "typeof price" in console ut will give us the data
 
+// NUMBER   
+
+// priceOfPen = 15; 
+
+// OPERATOR
+
+// assign , addition , subtraction , division , multiplication modulo operator
+
+// what is modulo operator(%) : if we have to find the remainder so we use modulo operator to find the remaineder
+
+// for example
+// 50 % 4 = 2 (so the remainder is 2)
+
+// EXPONENTIAL OPERATOR
+
+// it is used to find the power mean exponentioal 
+
+// for example
+// 3**2 = 9 
+
+// NaN means not a number. 
+// for example >> 0/0 , NaN +-*/1 these will not be a valid number
+
+// LET KEYWORD  
+// is defined as the syntax of declaring  a variable
+// let num1 = 2; let num2 = 4; let sum = num1 + num2; sum
+
+
+// CONSTANT KEYWORD 
+
+// is defined as the value that can't be changed mean fixed
+
+// const valueOfPi = 3.14; so if we want to change the value then it will give us errror
+
+// Task-1
+// find the area of circle
+
+// const pi = 3.14; const radius = 5;
+// const area = pi*radius**2;
+// area
+
+
+// ASSIGNMENT OPERATOR
+//  are binary operators
+
+// example >> let age = 34; age += 1: or age -=1; 
+
+// UNIRAY OPERATOR
+// to wrie in short form 
+
+// age++  >> increment "increase"
+
+// age--  >> decrement "decrease"
