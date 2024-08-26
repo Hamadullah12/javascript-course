defbe80b1c74572777db8cb42e3aa6a7fd7e3b03
Hamadullah12 <hamadfms05@gmail.com>
2024-08-26 12:27:00 +0500

    template  literal has been discussed

---
+++ Day-2/class-2.js
@@ -0,0 +1,14 @@
+// console.log("welcome to the class 2 of learning javascript")
+
+// let num1 = 23;
+// let num2 = 43;
+// console.log("Total sum of these values is :", num1+num2 );
+
+
+// TEMPLATE LITERAL
+// They are used to add embedded expressin in a string.
+
+// let cupPrice = 54;
+// let platePrice = 34;
+// let output = `the price is : ${cupPrice + platePrice} Rupees.` //backtick ``
+// console.log(output);
