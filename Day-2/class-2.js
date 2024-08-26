2805cc71c045a5b37d9991845177ba83089f0986
Hamadullah12 <hamadfms05@gmail.com>
2024-08-26 17:14:04 +0500

    operators are completed

--- Day-2/class-2.js
+++ Day-2/class-2.js
@@ -12,3 +12,64 @@
 // let platePrice = 34;
 // let output = `the price is : ${cupPrice + platePrice} Rupees.` //backtick ``
 // console.log(output);
+
+// OPERATORS
+
+// 1: Arithmetic operators
+
+// +,-,*,/,%,**
+
+// 2: Unary operator
+// ++a, a++, --a, a--
+
+// 3:Assignment operator
+
+// =, +=, -=, *=, /=, %=
+
+// 4:Comparison Operator
+
+// >, >=, <, <=, ==, !=, ===.
+// EXAMPLE
+
+// let age = 18 ;
+// console.log(age < 18);
+// console.log(age > 18);
+// console.log(age >= 18);
+// console.log(age != 18);
+// console.log(age == 18);
+
+// let a = 34;
+// let st = '34';
+// a == st The answer is true because it just look for value
+// a === st; 
+// so the ans is false
+
+// The case is sensitive in === it will also look for type means it compare both value and type
+
+
+// CONDITIONAL STATEMENTS
+
+// If statement
+
+
+// let age = 19;
+
+// if (age >= 18){
+//  console.log("you are eligible for vote")
+//     console.log("you are eligible for wedding")
+// }
+
+// let age = 13;
+
+// if(age >= 18){
+//     console.log("you can drive")
+// }
+//     if (age < 18){
+//         console.log("you cant't drive")
+//     }
+
+// let firstName = 'Hamad';
+
+// if(firstName == 'Hamad') { 
+//   console.log(`Welcome ${firstName} `);    
+// }
