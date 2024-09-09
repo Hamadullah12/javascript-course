451e26895c627a8d6815b014b527a68e321741e1
Hamadullah12 <hamadfms05@gmail.com>
2024-09-09 12:44:43 +0500

    loops by using prompt for tabels

--- Day-4/class-4.js
+++ Day-4/class-4.js
@@ -12,4 +12,30 @@
 // }
 
 // print all odd numbers from 1 to 15
+// One thing remember that whenever you need to sub or add greater than 1 so you have to equal the i+2  to i >> i= i+2;
+// for (let i = 1; i <= 15; i=i+2) {
+//     console.log(i);
+// }
 
+// FOR EVEN NUMBERS:
+
+// for (let even = 2; even <= 20; even=even+2){
+// console.log(even);
+// }
+// we can do the same for backward
+
+// PRINT 5 TABLE
+
+// for (let i = 5; i <= 50; i= i + 5) {
+//     console.log(i);
+// }
+
+
+// by using prompt we will take the valye from customer for the table
+// why we use parseInt : we use parseInt to convert string into integers.
+
+// let n = prompt('write any num for the tabel');
+// n = parseInt(n);
+// for (let i = n; i <= n*10; i = i+n) {
+//     console.log(i);
+// }
