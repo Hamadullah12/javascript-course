a36d1198f585dd126489dddee82c4bc79ad1e605
Hamadullah12 <hamadfms05@gmail.com>
2024-09-09 11:51:23 +0500

    problem having in console

---
+++ Day-4/class-4.js
@@ -0,0 +1,15 @@
+// LOOPS : is used to iterate (means repeat) piece of code 
+
+// 1: FOR LOOP:
+// for is loop keyword, let is the initialzation keyword, and i the ending , and i++ is the incrementation.
+// for (let i = 1; i <= 7; i++) {
+//     console.log(i);
+// }
+// backward loops
+
+// for (let i = 5; i >= 1; i--) {
+// console.log(i);
+// }
+
+// print all odd numbers from 1 to 15
+
