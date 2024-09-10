86db295dec6aae62f6d180a711d0f555d3e4c574
Hamadullah12 <hamadfms05@gmail.com>
2024-09-10 10:11:16 +0500

    guessing game program

--- Day-4/class-4.js
+++ Day-4/class-4.js
@@ -39,3 +39,46 @@
 // for (let i = n; i <= n*10; i = i+n) {
 //     console.log(i);
 // }
+
+// NESTED LOOPS:
+
+// for (let i = 1; i<=5; i++) {
+//     console.log(`outer loop ${i}`)
+//     for (let j = 1; j<=3; j++) {
+//         console.log(j);
+//     }
+// }
+
+// so by dry run we know that first i condtion will check then the all inner conditions will check and this process will happern for FIve 5 time
+// because i = 5 so after the value become false then the loop will stop there. so the inner loop will repeat for 5 times.
+
+// WHILE LOOP : The usage of both  loop are same
+// let i = 1;
+// while(i<=29) {
+//     console.log(i)
+//     i++;
+// }
+
+// FOR BACKWARD.
+// let i = 10;
+// while(i>=1) {
+//     console.log(i)
+//     i--;
+// }
+
+// We have to write a program for guessing our favt game.
+
+// let favGame = 'COD';
+// let userGuess = prompt('Guess our favt game ?');
+
+// while( (userGuess != favGame ) && (userGuess != "quit" )) {
+//     console.log("wrong");
+//     userGuess = prompt("try again");
+// }
+
+// if(userGuess == favGame) {
+//     console.log("thum thu bare tezz hu");
+// }else{
+//     console.log("Thum se na hopayga");
+// }
+
