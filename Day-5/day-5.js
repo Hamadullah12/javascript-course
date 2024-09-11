b607167e09ae01f58c6fea46ba111919fdd8d41a
Hamadullah12 <hamadfms05@gmail.com>
2024-09-11 11:06:14 +0500

    objects literals started

---
+++ Day-5/day-5.js
@@ -0,0 +1,20 @@
+// JS OBJECTS LITERALS: IT is a data structure that is  used to stored keyed collection and complex entities.
+// so in the below programs the syntax is clear that the key is "studentName" and value is the "Hamad Ullah", the sequence is not good but
+// it give us clear information insted of arrays and strings.
+
+// const studentData = {
+//     studentName : "Hamad Ullah",
+//     studentAge  : 19,
+//     studentMarks: 511,
+//     studentRollNum : 16113
+// };
+// console.log(studentData);
+
+// let grocery = {
+//     items : ['pens', 'pencils', 'soap', 'oil'],
+//     food  : ['chicken', 'shwarma', 'burger'],
+//     bill  : 5470,
+//     dicount : 470,
+//     totalBill : 5000
+// };
+// console.log(grocery);
