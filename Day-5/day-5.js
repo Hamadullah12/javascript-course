99487dedab40d2fa09575e027f614706729bea65
Hamadullah12 <hamadfms05@gmail.com>
2024-09-12 11:23:37 +0500

    objects literal get and add and update value

--- Day-5/day-5.js
+++ Day-5/day-5.js
@@ -18,3 +18,44 @@
 //     totalBill : 5000
 // };
 // console.log(grocery);
+
+// Create a thread/twitter post  by using objects literals
+
+// let socialPosts = {
+//     userName : "@hamad-Ullah07",
+//     content : "personal life",
+//     likes : 17,
+//     reposts: 0,
+//     tags : ['@ahmed-ali', '@jawad-ali']
+// };
+// console.log(socialPosts);
+
+// GET VALUES: To access the particular key in the objects literals
+
+
+// let socialPost = {
+//     userName : "@hamad-Ullah07",
+//     content : "personal life",
+//     likes : 17,
+//     reposts: 0,
+//     tags : ['@ahmed-ali', '@jawad-ali']
+// };
+// console.log(socialPost["userName"]);
+// second method
+
+// console.log(socialPost.content);
+
+// ADD UPDATE VALUES IN OBJECTS LITERALS
+
+const studentData = {
+    name : "Hamad Ullah",
+    class: 12,
+    result : "awaited",
+    city : "Peshawar"
+} ;
+// Add new property
+studentData.gender = "Male"
+// value update
+studentData.class = "graduated";
+studentData.result = "Pass";
+console.log(studentData);
