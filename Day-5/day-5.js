ab963da6b7a8c0611f770acc4442490eb91eebd4
Hamadullah12 <hamadfms05@gmail.com>
2024-09-17 12:17:22 +0500

    objects and math and guessing game done

--- Day-5/day-5.js
+++ Day-5/day-5.js
@@ -47,15 +47,148 @@
 
 // ADD UPDATE VALUES IN OBJECTS LITERALS
 
-const studentData = {
-    name : "Hamad Ullah",
-    class: 12,
-    result : "awaited",
-    city : "Peshawar"
-} ;
+// const studentData = {
+//     name : "Hamad Ullah",
+//     class: 12,
+//     result : "awaited",
+//     city : "Peshawar"
+// } ;
 // Add new property
-studentData.gender = "Male"
+// studentData.gender = "Male"
 // value update
-studentData.class = "graduated";
-studentData.result = "Pass";
-console.log(studentData);
+// studentData.class = "graduated";
+// studentData.result = "Pass";
+// console.log(studentData);
+
+// OBJECTS OF OBJECTS MEANS NESTED OBJECTS
+
+// let studentsData = {
+//     ahmed: {
+//         result: "pass",
+//         city: "Pindi"
+//     },
+
+//     jawad: {
+//         result: "pass",
+//         city: "Islamaabad"
+//     },
+
+//     hamad: {
+//         result: "pass",
+//         city: "Peshawar"
+//     }
+
+// }
+
+// console.log(studentsData);
+
+// ARRAY OF OBJECTS:
+
+// const studentsData = [
+//     {
+//         name: "ahmed",
+//         result: "pass",
+//         city: "Pindi"
+//     },
+
+//     {
+//         name: "jawad",
+//         result: "pass",
+//         city: "Islamaabad"
+//     },
+
+//     {
+//         name: "hamad",
+//         result: "pass",
+//         city: "Peshawar"
+//     }
+
+// ];
+// studentsData[1].result = "awaited";
+// console.log(studentsData);
+
+// MATH OBJECT: IT is used for different mathematical operatioms
+
+// console.log(Math.PI);
+// console.log(Math.E);
+
+// MATH OBJECT MEHTODS: 
+
+// 1: Math.abs(absolute : it is used to convert negative value into positive value)
+
+// console.log(Math.abs(-7));
+
+// 2: Math.pow(It is used to find the power of value)
+
+// console.log(Math.pow(7, 2))
+
+// 3: Math.floor (It is used to roundof the value to nearest of small value)
+
+// console.log(Math.floor(7.895697))
+
+// console.log(Math.floor(-7.895697))
+
+
+// 4:Math.ceil(it is used to round of to largest number means opposite to floor)
+
+// console.log(Math.ceil(7.895697));
+
+// Math.Random : it is used to pickup any number between 0 and 1 but not 1:
+
+// console.log(Math.random())
+// console.log(Math.random())
+// console.log(Math.random())
+// console.log(Math.random())
+// console.log(Math.random())
+// console.log(Math.random())
+// console.log(Math.random())      
+
+// RANDOM INTEGERS : We can generate random numbers or integers from the math method 
+// we have to generate random numbers from 0 to 20.
+// As we know that 1 never be come in random numbers so we will never get 20 for this we will add 1 with the math so after we add 1 we will
+// never get 0.
+
+// let finalQues = Math.floor(Math.random() * 20) + 1;
+// console.log(finalQues);
+// let finalQue = Math.floor(Math.random() * 20) + 1;
+// console.log(finalQue);
+
+// Generate a random num from 1 to 100.
+
+// let oneToHund = Math.floor(Math.random() * 100) + 1 ;
+// console.log(oneToHund);
+
+// from 0 to 10
+
+// let zeroToTen = Math.floor(Math.random() * 10) + 1;
+// console.log(zeroToTen );
+// Create a logic program in which user have to give maximum num and then he have to guess the number.
+
+// const maxGuess = prompt("Please enter any maximum number");
+
+// const random = Math.floor(Math.random() * maxGuess) + 1;
+
+// let userGuess = prompt("Now guess the correct number");
+
+// while(true) {
+//     if(userGuess == "quit") {
+//         console.log("user quit");
+//         break;
+//     }
+
+//     if(userGuess == random) {
+//         console.log("You are right, correct number is ", random);
+//         break;
+//     }
+
+//     else if(userGuess < random) {
+//         userGuess = prompt("your guess is small , please enter large num");
+//     }
+//     else if(userGuess > random) {
+//         userGuess = prompt("your guess is larger please enter small number");
+//     }
+
+//     else{
+//         userGuess = prompt("You are wrong . please try again");
+//     }
+// }
