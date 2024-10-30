55b8c6b8fb84e253b65d65c9f840c9c23abd8663
Hamadullah12 <hamadfms05@gmail.com>
2024-10-30 10:30:15 +0500

    Event bubbling done now start on todo list

---
+++ Day-11-Mini-projects/app.js
@@ -0,0 +1,23 @@
+// ==================================================================
+// ================== Event Bubbling Pr #1====================
+// let div = document.querySelector("div")
+// let ul = document.querySelector("ul")
+// let li = document.querySelectorAll("li")
+
+// div.addEventListener("click", function () {
+//     console.log("div was clicked")
+// })
+
+// ul.addEventListener("click", function (event) {
+//     event.stopPropagation();//to stop printing to the parent tree 
+//     console.log("ul was clicked")
+// })
+
+// for (lis of li){
+//     lis.addEventListener("click", function (event) {
+//         event.stopPropagation();
+//     console.log("li was clicked")
+// })
+// }
+
+// ======================================================================
