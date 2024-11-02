f2e954e5150bc1c4e41f1472861e26ae9456875f
Hamadullah12 <hamadfms05@gmail.com>
2024-11-02 09:56:05 +0500

    todo app has been created

--- Day-11-Mini-projects/app.js
+++ Day-11-Mini-projects/app.js
@@ -9,7 +9,7 @@
 // })
 
 // ul.addEventListener("click", function (event) {
-//     event.stopPropagation();//to stop printing to the parent tree 
+//     event.stopPropagation();//to stop printing to the parent tree
 //     console.log("ul was clicked")
 // })
 
@@ -19,5 +19,42 @@
 //     console.log("li was clicked")
 // })
 // }
+//         ===================End of evnet bubbling
+// ===========================TOdo LIst APP Pr #2===========================================
 
-// ======================================================================
+// let input = document.querySelector("input");
+// let btn = document.querySelector("button");
+// let ul = document.querySelector("ul");
+
+
+// btn.addEventListener("click", function () {
+//     let lists = document.createElement("li");
+//     lists.innerText = input.value;
+
+//     let delBtn = document.createElement("button")//for task deletion
+//     delBtn.innerText = "Delete"
+//     delBtn.classList.add("delete")
+
+//     lists.appendChild(delBtn);//to delete task
+
+//     ul.appendChild(lists);
+//     input.value = "";
+// })
+
+// we hace to use event bubblinh to delete the new tasks
+
+// ul.addEventListener("click", function (event) {
+  
+//"" console.dir(event.target.nodeName)//(so the target event is used to show us the\
+    //  identity of deleted task  and nodeName tells us due to which thing it has been executed)""
+
+    // now the real work now use parent element of event.target then romove propery by using if startement
+
+//     if (event.target.nodeName == "BUTTON") {
+//         let lisDele = event.target.parentElement;
+//         lisDele.remove();
+//         console.log(`deleted`)
+//     }
+// })
+
+// ==========================Todo list app pr #2 end=================================================
