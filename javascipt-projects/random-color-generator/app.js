cbb5f560f26ec0c144917a8e65f5f79ec6c2374a
Hamadullah12 <hamadfms05@gmail.com>
2024-11-06 15:49:28 +0500

    this project has been completed first we use arrow function for getting random number then merge it to strign and give unit 16 to get hexa value and then the rest of work

---
+++ javascipt-projects/random-color-generator/app.js
@@ -0,0 +1,36 @@
+// const getColor = () => {
+//     // for hex code
+//     const randomColor = Math.floor(Math.random() * 16777215);//the value that we use we get from google to make it to the hex code
+//     const randomCode = "#" + randomColor.toString(16);//concat the # with randomColor which will convert it to the string and 16 is the hexa unit
+//     console.log(randomCode)
+//     document.body.style.backgroundColor = randomCode;
+//     h2.innerText = randomCode;
+
+// }
+// let h2 = document.querySelector("h2")
+// let btn = document.querySelector("button")
+
+// // event
+// btn.addEventListener("click", getColor)
+
+// getColor()
+
+
+// =================================Practice by myself============================================
+
+const generateRandomColor = () => {
+    const randomCode = Math.floor(Math.random() * 16777215)
+    const randomColor = "#" + randomCode.toString(16);
+    console.log(randomColor)
+    h2.innerText = randomColor;
+    document.body.style.backgroundColor = randomColor;
+}
+
+
+let btn = document.querySelector("button");
+let h2 = document.querySelector("h2")
+
+
+btn.addEventListener("click", generateRandomColor)
+
+generateRandomColor()
