da5a73e1632c2ff528cecb671694305bae088dfc
Hamadullah12 <hamadfms05@gmail.com>
2024-12-06 11:54:27 +0800

    our first request by using fetch() has been done

---
+++ Day-13-Async/request.js
@@ -0,0 +1,40 @@
+// OUr first request
+// using fetch (url)
+
+// let url = "https://catfact.ninja/fact/";
+// fetch(url)// as this fetch will return us a promise so we will  use promise methods
+//     .then((res) => {
+//         console.log(res)//so here we will get the information but all headers are not responding so we have to get the readealbe stream
+//         return res.json();
+//     }).then((data) => {
+//         console.log(data.fact);//so we will get the body portion which is not showing in respone readabel stream
+//     })
+    
+//     .catch((err) => {
+//     console.log("Error -,", err)
+// })
+
+
+let url = "https://catfact.ninja/fact/";
+fetch(url)
+    .then((res) => {
+        console.log(res);
+        return res.json();
+    })
+    .then((data) => {
+        console.log("Data 1: ", data.fact);
+        return fetch(url);
+    }).then((res) => {
+        return res.json();
+    }).then((data2) => {
+        console.log("Data 2:", data2.fact);
+    })
+    .catch((err) => {
+    console.log("Error Caught -", err)
+    })
+
+// so this is our first request
+    
+// first we fetch url then it give us promise then we use then(res) to get the response and
+// then we convert it to the json by res.json(), after that we get our first data by using then()chaning method
+// as the same process ocuur above .
