bdc7812c310d520b41aa8f75bdcbaf9d020e6dd4
Hamadullah12 <hamadfms05@gmail.com>
2024-12-10 13:02:36 +0800

    dog api done

--- Day-14-Axios/app.js
+++ Day-14-Axios/app.js
@@ -20,25 +20,50 @@
 // NOw we have to create a program in which when we click on the button so the fact should dis
 // -play in the paragraph means on webpage .
 
-let button = document.querySelector("button");
-let p = document.querySelector("#storeFact");
+// let button = document.querySelector("button");
+// let p = document.querySelector("#storeFact");
 
-button.addEventListener("click", async () => {//as we call asyn function in evnet so we have to use asyn and await.
-    let fact = await getFacts();
-    p.innerText = fact;
-})
-let url = "https://catfact.ninja/fact/";
+// button.addEventListener("click", async () => {//as we call asyn function in evnet so we have to use asyn and await.
+//     let fact = await getFacts();
+//     p.innerText = fact;
+// })
+// let url = "https://catfact.ninja/fact/";
 
-async function getFacts() {
-    try {
-        let res = await axios.get(url);
-        return (res.data.fact);
-    } catch (e) {
-        console.log("Error caught;", e)
-        return ("No response here");
-    }
-}
+// async function getFacts() {
+//     try {
+//         let res = await axios.get(url);
+//         return (res.data.fact);
+//     } catch (e) {
+        
+//         console.log("Error caught;", e)
+//         return ("No response here");
+//     }
+// }
 
 // So in this program we get the random facts of cats
 //so the procedure will be that when we clikced on the button it will call the getFacts() function
 // and it will wait until the function completed then it wiill show us the result in the paragraph
+
+// Now create a program of getting dog images from api
+
+let button = document.querySelector("button");
+let img = document.querySelector("#result");
+
+button.addEventListener("click", async () => {
+    let imageRes = await getImages();
+    img.setAttribute("src", imageRes);//we use set attributes when we have source or links 
+    console.log(imageRes);
+})
+
+let url2 = "https://dog.ceo/api/breeds/image/random";
+async function getImages() {
+    try {
+        let res = await axios.get(url2)
+     return res.data.message;
+    } catch (e) {
+        console.log("Error caught;", e);
+        return "Image not found";
+    }
+}
+
+// so by this way we get multiple dog images.
