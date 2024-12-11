2067f823fdc4419c9154123cf31e31888316a30c
Hamadullah12 <hamadfms05@gmail.com>
2024-12-11 10:57:11 +0800

    Query string program has been done for university

--- Day-14-Axios/app.js
+++ Day-14-Axios/app.js
@@ -87,4 +87,52 @@
 // }
 // ===========================================================================
 // Updating Query string in Axios :
+// We have two tasks
+// 1: FIrst we have to create a program in which user search for country and it display the
+// names of colleges of that country.
+// 2: The second task is that you have to then fixed any country and then the user have to
+// search for state in which state he wants to find colleges .
+// so first take university api then start the project
+
+
+let url = "http://universities.hipolabs.com/search?name="
+//we empty name because we have to add it with url and from input search user will search for that
+let button = document.querySelector("button");
+let lists = document.querySelector("#collLists")
+
+button.addEventListener("click", async () => {
+    let country = document.querySelector("input").value;
+    console.log(country);
+
+    let collArr = await getColleges(country);
+    // here we will take call back for returing college name in list 
+    showList(collArr);
+
+})
+function showList(collArr) {
+    // now we are creating lists in ul to show college on webpage
+    let collLists = document.querySelector("#collLists");
+    collLists.innerText=""
+    //now we will take for of loop for each  college name 
+    for (col of collArr) {
+// list item created here
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        collLists.append(li);
+    }
+
+}
+
+// let country = "India";
+
+async function getColleges(country) {
+    try {
+        let res = await axios.get(url + country);
+        return res.data;
+    } catch (e) {
+        console.log("Error caught;", e);
+        return [];
+    }
+}
+
 
