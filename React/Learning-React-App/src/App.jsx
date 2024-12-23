0e9ac59e53eeb3f86d4108dc888abf1dccebaf5f
Hamadullah12 <hamadfms05@gmail.com>
2024-12-23 09:46:06 +0800

    We create multiple component separetly and done import and export

--- React/Learning-React-App/src/App.jsx
+++ React/Learning-React-App/src/App.jsx
@@ -1,7 +1,25 @@
-import "./App.css"
+import "./App.css";
 
+import {Test1} from "./Test.jsx";
+import {Multi} from "./Test.jsx";
+
+
+function TestDes() {
+    return <p>Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Eligendi voluptatibus adipisci, molestiae facere iusto
+        numquam enim beatae nesciunt itaque. Soluta!</p>
+}
 function App() {
-return <button>Hellow Click me bros</button>
+    return (
+        <div>
+            <Test1 />
+            <Multi/>
+            <Multi/>
+            <TestDes/>
+            <Test1/>
+            <TestDes/>
+        </div>
+    ) 
 }
 
-export default App
+export default App;
---
+++ React/Learning-React-App/src/Test.jsx
@@ -0,0 +1,11 @@
+// import "./App.css";
+function Test1() {
+    return <h1>Hellow every one from Ham d gamer by Programmer </h1>
+
+}
+
+function Multi() {
+    return <h3>I am multiper value</h3>
+}
+export { Test1, Multi };
+
