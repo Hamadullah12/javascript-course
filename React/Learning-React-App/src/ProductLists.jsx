8a9f69af8865ad63227fc8e890933f06d06fcf38
Hamadullah12 <hamadfms05@gmail.com>
2024-12-28 09:54:06 +0800

    customizing on the basis of condtions

--- React/Learning-React-App/src/ProductLists.jsx
+++ React/Learning-React-App/src/ProductLists.jsx
@@ -1,9 +1,7 @@
 import Products from "./Products";
 function ProductsList() {
-    let features = ["soft", "good-stuff", "neat-and-clean"];
     return (
         <>
-     {/* <Products title="Mens Clothes"  feature = {features}/> */}
      <Products title = "Mens Clothes" price = {1000} />
      <Products title = "Womens Clothes" price = {5000} />
      <Products title = "Womens Clothes" price = {10000} />
--- React/Learning-React-App/src/Products.jsx
+++ React/Learning-React-App/src/Products.jsx
@@ -1,11 +1,11 @@
 import "./Products.css";
-function Products({title, price, feature}) {
+function Products({ title, price }) {
+    let isDiscount = price > 6000; 
+    let styles = { backgroundColor: isDiscount ? "red" : "" };
     return (
-        <div className="Products">
+        <div className="Products" style = {styles}>
             <h3>{ title }</h3>
             <h5>Price is {price}</h5>
-            {/* <p>{feature.map((feature) =>
-                <li>{feature} </li>)}</p> */}
             {price > 2000 && <p>Discount 26%</p>}
     </div>
     )
