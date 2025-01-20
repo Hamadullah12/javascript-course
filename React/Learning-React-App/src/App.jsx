9ae333f15f55ed3b1ffbf68de9f6561101bb60c8
Hamadullah12 <hamadfms05@gmail.com>
2025-01-20 10:24:22 +0800

    props in react by code stoic

--- React/Learning-React-App/src/App.jsx
+++ React/Learning-React-App/src/App.jsx
@@ -3,11 +3,14 @@ import Products  from "./Products.jsx";
 import ProductsList from "./ProductLists.jsx";
 import Button from "./Button.jsx";
 import FormSubmit from "./Form.jsx";
+import Hello from "./React-Code-Stoic/Props.jsx";
 
 function App() {
     return (
         <>
-       <FormSubmit/>
+       <Hello Hello="Hi there everyone" message = "How are you all"/>
+       <Hello Hello="Hi there everyone" message = "How are "/>
+       <Hello Hello="Hi there everyone" message = "How are all"/>
         </>
     ) 
 }
---
+++ React/Learning-React-App/src/React-Code-Stoic/Props.jsx
@@ -0,0 +1,5 @@
+export default function Hello(Props) {
+    return (
+        <h1>{Props.Hello} {Props.message}</h1>
+    )
+}
