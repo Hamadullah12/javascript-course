e8f90a3e2aa71bcc3c40626c2e1df82bde09c81c
Hamadullah12 <hamadfms05@gmail.com>
2025-01-23 15:24:24 +0800

    form has been created and input fields are done

--- React/Learning-React-App/src/React-Code-Stoic/Form.jsx
+++ React/Learning-React-App/src/React-Code-Stoic/Form.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react"
 export default function Form() {
-    const [name, setName] = useState("");
-   
+    const [name, setName] = useState({firstName : "", lastName: ""});
+    function handleClick(e) {
+        e.preventDefault();
+        console.log({name})
+   }
     return (
         <div>
+            {name.firstName} -  {name.lastName}
             <form>
                 <input onChange={(e) => {
-                    setName(e.target.value)
-                }} type="text" value={name} />
+                    setName({...name, firstName: e.target.value })
+                }} type="text" value={name.firstName} />
+                <input onChange={(e) => {
+                    setName({...name, lastName: e.target.value })
+                }} type="text" value={name.lastName} />
+                <button onClick={(e)=>handleClick(e)}>Submit</button>
             </form>
         </div>
     )
