c502a22f19f016d58ddbb89ea1eef1d091e6c199
Hamadullah12 <hamadfms05@gmail.com>
2025-01-30 12:28:51 +0800

    we displau data of api using state varivale in app and search

--- React/Food-Recipe/Food-Recipe/src/Components/Search.jsx
+++ React/Food-Recipe/Food-Recipe/src/Components/Search.jsx
@@ -1,15 +1,25 @@
-import { useEffect, useState } from "react"
-
-export default function Search() {
-    // syntax for useEffect()
-    useEffect(() => {
-        function test() {
-            console.log("Test has been executed")
-        }
-        test(),[query]
-    })
-    const [query, setQuery]=useState("pizza...")
-    return <div>
-        <input value={query} onChange={(e)=>setQuery(e.target.value)} type="text" />
+import { useEffect, useState } from "react";
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "8a6c34f362d5440b96fc21728ae42576";
+export default function Search({ foodData, setFoodData }) {
+  const [query, setQuery] = useState("pizza...");
+  // syntax for useEffect()
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+      setFoodData(data.results);
+    }
+    fetchFood();
+  }, [query]);
+  return (
+    <div>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      />
     </div>
+  );
 }
