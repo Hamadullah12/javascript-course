22c04c085675e9bb0935133c0ded236d051aca4a
Hamadullah12 <hamadfms05@gmail.com>
2025-01-30 09:39:29 +0800

    we make it soreted the items and the todo app is completeyly done completed

--- React/Todo-App/Todo-App/src/components/TodoList.jsx
+++ React/Todo-App/Todo-App/src/components/TodoList.jsx
@@ -1,10 +1,11 @@
 import TodoItem from "./Todoitem";
 import style from "./todoList.module.css"
-export default function TodoList({todos,setTodos}) {
+export default function TodoList({ todos, setTodos }) {
+  const sortedItem = todos.slice().sort((a, b)=> Number(a.done)-Number(b.done))
     return (
         <div className={style.list}>
           {
-        todos.map((item) => (
+        sortedItem.map((item) => (
           <TodoItem key={item.name} item={item} todos={todos} setTodos={setTodos} />
         ))}
         </div>
