2110a5d713c3e8de52b05aab2c3b2e488b562a33
Hamadullah12 <hamadfms05@gmail.com>
2025-02-03 21:57:40 +0800

    preparing the details for rexipes

--- React/Food-Recipe/Food-Recipe/src/App.jsx
+++ React/Food-Recipe/Food-Recipe/src/App.jsx
@@ -1,15 +1,26 @@
+import "./App.css";
 import { useState } from "react";
 import Search from "./Components/Search";
 import FoodList from "./Components/FoodList";
 import Nav from "./Components/Nav";
-import "./App.css";
+import Container from "./Components/Container";
+import InnerContainer from "./Components/InnerContainer";
+import FoodDetail from "./Components/FoodDetail";
 function App() {
   const [foodData, setFoodData] = useState([]);
+  const[foodId, setFoodId]=useState("");
   return (
     <>
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
-      <FoodList foodData={foodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList setFoodId={setFoodId} foodData={foodData} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetail  foodId={foodId}/>
+        </InnerContainer>
+      </Container>
     </>
   );
 }
--- React/Food-Recipe/Food-Recipe/src/Components/Search.jsx
+++ React/Food-Recipe/Food-Recipe/src/Components/Search.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import styles from "./search.module.css";
 const URL = " https://api.spoonacular.com/recipes/complexSearch";
-const API_KEY = "8a6c34f362d5440b96fc21728ae42576";
+// const API_KEY = "8a6c34f362d5440b96fc21728ae42576";
+const API_KEY = "b7804eefd6934db1910edc54793d4c1e";
 export default function Search({ foodData, setFoodData }) {
-  const [query, setQuery] = useState("pizza...");
+  const [query, setQuery] = useState("drink");
   // syntax for useEffect()
   useEffect(() => {
     async function fetchFood() {
@@ -14,8 +16,9 @@ export default function Search({ foodData, setFoodData }) {
     fetchFood();
   }, [query]);
   return (
-    <div>
+    <div className={styles.searchContainer}>
       <input
+        className={styles.inputContainer}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         type="text"
